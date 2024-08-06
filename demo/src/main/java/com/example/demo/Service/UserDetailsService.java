7b79491c4fcf61e8898fecdfc89046065cbcfb12
praveen-dass <praveenjayaramdass@gmail.com>
2024-08-06 14:00:23 +0530

    changed userdetail

--- demo/src/main/java/com/example/demo/Service/UserDetailsService.java
+++ demo/src/main/java/com/example/demo/Service/UserDetailsService.java
@@ -22,20 +22,20 @@ public class UserDetailsService {
     }
 
     public UserDetails updateUser(String username, UserDetails userDetails) {
-        Optional<UserDetails> existingUserDetails = userDetailsRepository.findById(username);
+         Optional<UserDetails> existingUserDetails =
+         userDetailsRepository.findById(username);
 
-        if (existingUserDetails.isPresent()) {
-            UserDetails updateUserDetails = existingUserDetails.get();
-            updateUserDetails.setName(userDetails.getName());
-            updateUserDetails.setEmail(userDetails.getEmail());
-            updateUserDetails.setPhone(userDetails.getPhone());
-            updateUserDetails.setJobrole(userDetails.getJobrole());
-            updateUserDetails.setAbout(userDetails.getAbout());
+         if (existingUserDetails.isPresent()) {
+         UserDetails updateUserDetails = existingUserDetails.get();
+         updateUserDetails.setName(userDetails.getName());
+         updateUserDetails.setEmail(userDetails.getEmail());
+         updateUserDetails.setPhone(userDetails.getPhone());
+         updateUserDetails.setJobrole(userDetails.getJobrole());
+         updateUserDetails.setAbout(userDetails.getAbout());
 
-            return userDetailsRepository.save(updateUserDetails);
-        }
+        return userDetailsRepository.save(userDetails);
+         }
 
-        return null;
+         return null;
     }
 }
-
